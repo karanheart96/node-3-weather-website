f1c5bf87fa2236ef4cb2a79962cec867d48c2bd9
HEARTLIN KARAN MACHADO <50643749+karanheart96@users.noreply.github.com>
2020-01-04 01:14:19 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about',(req,res) => {
 
 app.get('/help',(req,res) => {
     res.render('help', {
-        helpText:'This is some useful Text',
+        helpText:'Contact Karan,MSCS student at Northeastern University,Boston,Massachussets.',
         title:'Help',
         name:'Karan'
     })
